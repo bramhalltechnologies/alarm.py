3daa305a21067ed5c110de5268cc6a1c2d2693a7
bramhalltechnologies <harrison.bramhall@tufts.edu>
2022-02-21 17:00:28 -0500

    Ming's Source Code

--- alarm.py
+++ alarm.py
@@ -1,4 +1,5 @@
 # Author: Harrison Bramhall
+#   Skeleton Provided By: Ming Chow - Tufts University
 # Date: 2/21/2022
 # Description: Intro to Security Class - Project 4 Using Python and scapy, write a program named alarm.py
 #   that provides user the option to analyze a live stream of network packets or a set of PCAPs for incidents.
@@ -10,4 +11,33 @@
 #       - Nikto scan
 #       - Someone scanning for Remote Desktop Protocol (RDP) protocol
 
+#!/usr/bin/python3
 
+from scapy.all import *
+import argparse
+
+def packetcallback(packet):
+  try:
+    # The following is an example of Scapy detecting HTTP traffic
+    # Please remove this case in your actual lab implementation so it doesn't pollute the alerts
+    if packet[TCP].dport == 80:
+      print("HTTP (web) traffic detected!")
+  except:
+    pass
+
+parser = argparse.ArgumentParser(description='A network sniffer that identifies basic vulnerabilities')
+parser.add_argument('-i', dest='interface', help='Network interface to sniff on', default='eth0')
+parser.add_argument('-r', dest='pcapfile', help='A PCAP file to read')
+args = parser.parse_args()
+if args.pcapfile:
+  try:
+    print("Reading PCAP file %(filename)s..." % {"filename" : args.pcapfile})
+    sniff(offline=args.pcapfile, prn=packetcallback)    
+  except:
+    print("Sorry, something went wrong reading PCAP file %(filename)s!" % {"filename" : args.pcapfile})
+else:
+  print("Sniffing on %(interface)s... " % {"interface" : args.interface})
+  try:
+    sniff(iface=args.interface, prn=packetcallback)
+  except:
+    print("Sorry, can\'t read network traffic. Are you root?")
