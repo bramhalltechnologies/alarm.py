4a000563acc751a6a2566e1bb213cfa597a37af4
bramhalltechnologies <harrison.bramhall@tufts.edu>
2022-02-21 16:10:36 -0500

    Header Added

--- alarm.py
+++ alarm.py
@@ -1,1 +1,13 @@
-#hello, world!
+# Author: Harrison Bramhall
+# Date: 2/21/2022
+# Description: Intro to Security Class - Project 4 Using Python and scapy, write a program named alarm.py
+#   that provides user the option to analyze a live stream of network packets or a set of PCAPs for incidents.
+#   Your tool shall be able to analyze for the following incidents:
+#       - NULL scan
+#       - FIN scan
+#       - Xmas scan
+#       - Usernames and passwords sent in-the-clear via HTTP Basic Authentication, FTP, and IMAP
+#       - Nikto scan
+#       - Someone scanning for Remote Desktop Protocol (RDP) protocol
+
+
